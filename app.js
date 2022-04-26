e9b4fee2511dfb552a2ece47c125d36e96392add
macnemoto <marcosj465@gmail.com>
2022-04-26 12:12:35 +0800

    :wrench: update the code

--- app.js
+++ app.js
@@ -1,10 +1,91 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const con = require('./database.js')
 const app = express()
 const port = 3000
 
+app.use(bodyParser.urlencoded({extended: true}))
 
+app.use(bodyParser.json())
+
+
+
+
+app.post("/cards", function (req, res) {
+  let newCards = { ...req.body };
+
+  con.query("INSERT INTO cards SET ?", newCards, (error, result) => {
+    if (error) {
+      return res.status(500).json({ status: "ERROR", error });
+    }
+
+    return res.json({ status: "SUCCESS" });
+  });
+});
+
+// Get all cards in database
 app.get('/', (req, res) => {
+
+
+  con.query("SELECT * from cards", (error, result) => {
+    if (error) {
+      return res.json({ status: "ERROR", error });
+    }
+
+    return res.json(result);
+  });
+    
+})
+
+
+// Get a cars by id
+app.get('/:id', (req, res) => {
+
+
+  con.query("SELECT * from cards WHERE id = ?", [req.params.id] ,(error, result) => {
+    if (error) {
+      return res.json({ status: "ERROR", error });
+    }
+
+    return res.json(result);
+  });
+    
+})
+
+// Get a cars by name
+app.get('/pokemon/:name', (req, res) => {
+
+
+  con.query("SELECT * from cards WHERE name = ?", [req.params.name] ,(error, result) => {
+    if (error) {
+      return res.json({ status: "ERROR", error });
+    }
+
+    return res.json(result);
+  });
+    
+})
+
+// Delete by id 
+app.delete('/:id', (req, res) => {
+
+
+  con.query("DELETE * from cards WHERE id = ?", [req.params.id] ,(error, rows) => {
+    if (!error) {
+    res.send(`Cards with the record Id: ${[req.params.id]} has been removed.`);
+    }
+
+    console.log(error);
+  });
+    
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})  
+
+//result[2].img `<br>` + result[2].name
+
   /*
   res.json({
       "data":{
@@ -13,22 +94,3 @@ app.get('/', (req, res) => {
 "img": "https://images.pokemontcg.io/base1/58.png"
 }
   })*/
-
-  res.send (
-  
-
-    con.connect(function(err) {
-      if (err) throw err;
-      con.query("SELECT * FROM cards", function (err, result, fields) {
-        if (err) throw err;
-        console.log(result[2].img);
-      });
-    })
-
-    
-  );
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})  
